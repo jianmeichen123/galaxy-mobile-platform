9e3ca186c28d5bcad46acd4bd7eca6e73fa05143
gxc <xiangchengao@galaxyinternet.com>
2016-08-10 12:37:00 +0800

    修改消息提醒

--- galaxy-mobile-platform-api/src/main/java/com/galaxyinternet/model/operationMessage/OperationType.java
+++ galaxy-mobile-platform-api/src/main/java/com/galaxyinternet/model/operationMessage/OperationType.java
@@ -14,7 +14,7 @@ public enum OperationType {
 	_6_1_			("/galaxy/project/ap",		PlatformConst.PROJECT_TYPE, "进入接触访谈阶段"),
 	//_6_1_			("/galaxy/project/ap",		PlatformConst.PROJECT_TYPE, "进入接触访谈阶段"),
 	/*UPDATE					("/galaxy/project/up", 				PlatformConst.PROJECT_TYPE, "编辑"), */
-	UPDATE					("/galaxy/project/up", 	PlatformConst.PROJECT_TYPE, "编辑"),
+	UPDATE					("/galaxy/project/editProject", 	PlatformConst.PROJECT_TYPE, "编辑"),
 	/*PRO_ADD_PER 			("/galaxy/project/app", 			PlatformConst.PROJECT_TYPE, "添加团队成员"), 
 	PRO_EDIT_PER			("/galaxy/project/upp", 			PlatformConst.PROJECT_TYPE, "修改团队成员"), 
 	PRO_DEL_PER 			("/galaxy/project/dpp", 			PlatformConst.PROJECT_TYPE, "删除团队成员"), */
